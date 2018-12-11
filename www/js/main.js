8eb12100eab0bf395435fda3ccc01d1a58ed46ad
John Thomure <johnthomure@Johns-MacBook-Pro.local>
2018-12-11 15:26:26 -0600

    Tweening Added

--- www/js/main.js
+++ www/js/main.js
@@ -9,11 +9,8 @@ function setup(){
 
   scene = new THREE.Scene()
 
-
-
   camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 0.1, 1000)
   //camera.position.z = (window.innerWidth/ window.innerHeight)
-
 
 
   let geometry = new THREE.BoxGeometry(80, 20, 80)
@@ -30,6 +27,7 @@ function setup(){
 
 function draw(){
   background(0)
+  TWEEN.update()
   renderer.render(scene,camera)
 }
 
@@ -41,42 +39,65 @@ function onDocumentKeyPress(){
   //a
     if (keyCode==97) {
       camera.position.x -= positionU
+        new TWEEN.Tween(camera.position)
+        .to({x: camera.position.x-0.2}, 1000)
+        .start()
     }
+
   //d
     else if (keyCode==100){
       camera.position.x += positionU
+        new TWEEN.Tween(camera.position)
+        .to({x: camera.position.x+0.2}, 1000)
+        .start()
     }
+
   //w
     else if (keyCode==119){
       camera.position.z -= positionU
+        new TWEEN.Tween(camera.position)
+        .to({z: camera.position.z-0.2}, 1000)
+        .start()
     }
+
   //s
     else if (keyCode==115){
       camera.position.z += positionU
+        new TWEEN.Tween(camera.position)
+        .to({z: camera.position.z+0.2}, 1000)
+        .start()
     }
-  //q
-    else if (keyCode==113){
-      camera.position.y += positionU
-    }
+
   //e
     else if (keyCode==101){
       camera.position.y -= positionU
+        new TWEEN.Tween(camera.position)
+        .to({y: camera.position.y-0.2}, 1000)
+        .start()
     }
-  // //t
-  // 	else if (keyCode==116){
-  // 		camera.rotation.x += rotationU
-  // 	}
-  // //g
-  // 	else if (keyCode==103){
-  // 		camera.rotation.x -= rotationU
-  //	}
+
+  //q
+    else if (keyCode==113){
+      camera.position.y += positionU
+        new TWEEN.Tween(camera.position)
+        .to({y: camera.position.y+0.2}, 1000)
+        .start()
+      }
+
   //f
     else if (keyCode==102){
       camera.rotation.y += rotationU
+        new TWEEN.Tween(camera.rotation)
+        .to({y: camera.rotation.y+0.2}, 1000)
+        .start()
     }
+
   //h
     else if (keyCode==104){
       camera.rotation.y -= rotationU
+        new TWEEN.Tween(camera.rotation)
+        .to({y: camera.rotation.y-0.2}, 1000)
+        .start()
     }
 
 }
